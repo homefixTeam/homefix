2ddb044a079c49ce49bdaff076ec4b336863a9d2
liuxuewen <liuxuewen@souyidai.com>
2015-10-18 08:37:53 +0800

    装修服务选择项修改

--- js/fw.js
+++ js/fw.js
@@ -7,13 +7,22 @@ $(function(){
 		var liHtml = '<li class="cf">' + $parLi.html().replace('fa-plus', 'fa-minus') + '</li>';
 		$parLi.after(liHtml);
 
-	})
-
-	$('.fw-cal-par').on('touchend', '.fa-minus-square-o', function(){
+	}).on('touchend', '.fa-minus-square-o', function(){
 		var $this = $(this),
 			$parLi = $this.closest('li');
 
 		$parLi.remove();
+
+	}).on('touchend', '.icon-check', function(){
+		//选择框
+		var $this = $(this);
+		$checkPar = $this.closest('.fw-checked');
+		$checkPar.find('.icon-check').addClass('uncheck');
+
+		$this.removeClass('uncheck');
+	}).on('touchend', '.fw-checked span', function(){
+
+		$(this).prev().trigger('touchend');
 
 	})
 
