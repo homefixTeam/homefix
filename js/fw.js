3db5ba7b8b3e25ace4cf00ee228f77d88a6c511f
liuxuewen <liuxuewen@souyidai.com>
2015-12-22 22:54:10 +0800

    修改样式

--- js/fw.js
+++ js/fw.js
@@ -1,4 +1,51 @@
 $(function(){
+
+	var vali_Obj = {
+		"txtLength":{
+			n: "请输入长度",
+			t: "长度不超过{0}字",
+			g: "请输入正确的长度格式"
+		},
+		"txtHeight":{
+			n: "请输入高度",
+			t: "高度不能超过{0}",
+			g: "请输入正确的高度格式"
+		},
+		"txtWidth":{
+			n: "请输入宽度",
+			t: "宽度不能超过{0}",
+			g: "请输入正确的宽度格式"
+		}
+	};
+	
+	$(".fw-cal-par li").find('input').on("blur",function(){
+
+		$(this).closest('li').removeClass('input-error');
+
+		var dataVali = $(this).attr("data-vali");
+		var dataMax = $(this).attr("data-max");
+		var reg = /\d+/g;
+		var thisVal = $(this).val();
+
+		if(thisVal==""){
+			setTip.call(this, 'n');
+		}else if(!reg.test(thisVal)){
+			setTip.call(this, 'g');
+		}else if(parseInt(thisVal) > parseInt(dataMax)){
+			setTip.call(this, 't', dataMax);
+		}
+
+		function setTip(type, replaceVal){
+			var _errorMsg = vali_Obj[dataVali][type];
+			if(replaceVal){
+				_errorMsg = _errorMsg.replace(/\{0\}/g, replaceVal);
+			}
+			$(this).closest('li').addClass('input-error');
+			$(this).closest("li").find('.input-tip').html(_errorMsg);
+		}
+		
+	});
+
 	//add
 	$('.fw-cal-par').on('touchend', '.fa-plus-square-o', function(){
 		var $this = $(this),
